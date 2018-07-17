7ae17a2f67a1eb673af9d22f91d78ad1f61b2c0e
mattkyea <kyeamatt@gmail.com>
2018-07-17 17:39:23 -0400

    identifies song, pulling artist, title, and spotify ID from returned JSON

---
+++ identify.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+'''
+    >>> python test.py test.mp3
+'''
+
+import os, sys
+from acrcloud.recognizer import ACRCloudRecognizer
+from acrcloud.recognizer import ACRCloudRecognizeType
+
+if __name__ == '__main__':
+    config = {
+        'host':'identify-us-west-2.acrcloud.com',
+        'access_key':'e00fce65717e122400eab1f48bc3a56b',
+        'access_secret':'secret',
+        'recognize_type': ACRCloudRecognizeType.ACR_OPT_REC_AUDIO, # you can replace it with [ACR_OPT_REC_AUDIO,ACR_OPT_REC_HUMMING,ACR_OPT_REC_BOTH], The     SDK decide which type fingerprint to create accordings to "recognize_type".
+        'debug':False,
+        'timeout':10 # seconds
+    }
+    
+    '''This module can recognize ACRCloud by most of audio/video file. 
+        Audio: mp3, wav, m4a, flac, aac, amr, ape, ogg ...
+        Video: mp4, mkv, wmv, flv, ts, avi ...'''
+    re = ACRCloudRecognizer(config)
+
+    #recognize by file path, and skip 0 seconds from from the beginning of sys.argv[1].
+    json = re.recognize_by_file(sys.argv[1], 0, 10)
+    print(json)
+    start = json.index("\"artists\":[{\"name\":") + 20 ##20 chars before the stuff we want
+    end = json.index("\"",start)
+    artist = json[start:end]
+    start = json.index("\"title\":\"") + 9
+    end = json.index("\"",start)
+    title = json[start:end]
+    start = json.index("spotify")
+    if start != 1:
+        json=json[start:]
+        json = json[json.index("track"):]
+        start = json.index("\"id\":\"") + 6
+        end = json.index("\"",start)
+        songId = json[start:end]
+        ##return songId
+        print(songId + ", "+ title + " by " + artist)
+    else:
+        ##return "Not on Spotify, but song is " + title + " by " + artist
+        print("Not on Spotify, but song is " + title + " by " + artist)
